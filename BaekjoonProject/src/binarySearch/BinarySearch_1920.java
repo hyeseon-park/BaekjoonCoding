54b70d30fa346a1cdcbde150909cff59d3df6984
hyeseon-park <lightning350@naver.com>
2020-03-16 15:51:53 +0900

    BinarySearch

---
+++ BaekjoonProject/src/binarySearch/BinarySearch_1920.java
@@ -0,0 +1,38 @@
+package binarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch_1920 {
+	static int[] nArr;
+	static int n;
+	static int m;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		nArr = new int[n];
+		for(int i=0; i<n; i++) {
+			nArr[i] = scan.nextInt();
+		}
+		Arrays.sort(nArr); //오름차순
+		m = scan.nextInt();
+		for(int i=0; i<m; i++) {
+			System.out.println(binarySearch(nArr, scan.nextInt()));
+		}
+	}
+	public static int binarySearch(int[] nArr, int target) { 
+		int high = nArr.length-1;
+		int low = 0;
+		while(high >= low) {
+			int mid = (high+low)/2;
+			if(nArr[mid] == target) {
+				return 1;
+			} else if(nArr[mid] > target) {
+				high = mid-1;
+			} else if(nArr[mid] < target) {
+				low = mid+1;
+			}
+		}
+		return 0;
+	}
+}
---
+++ BaekjoonProject/src/binarySearch/BinarySearch_2022.java
@@ -0,0 +1,40 @@
+package binarySearch;
+
+import java.util.Scanner;
+
+public class BinarySearch_2022 {
+	static double x;
+	static double y;
+	static double c;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		x = scan.nextDouble();
+		y = scan.nextDouble();
+		c = scan.nextDouble();
+		if(x<y) {
+			double tmp = x;
+			x = y;
+			y = tmp;
+		} //x>y
+		double l = 0; //최소값
+		double r = y; //최대값 x>y>d
+		while(true) { //binary search
+			double d = (l+r)/2; //중간값
+			double tmpC = binarySearch(d);
+			if(Math.abs(tmpC-c)<=0.001) {
+				System.out.printf("%.3f", d);
+				return;
+			}
+			if(tmpC > c) {
+				l = d;
+			} else if(tmpC < c) {
+				r = d;
+			}
+		}
+	}
+	public static double binarySearch(double d) { 
+		double hx = Math.sqrt(x*x-d*d); 
+		double hy = Math.sqrt(y*y-d*d);
+		return (hx*hy)/(hx+hy);
+	}
+}
