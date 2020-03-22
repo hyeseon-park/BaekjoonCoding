489f23eb16a011f10325bb73f198c68e49eb5256
hyeseon-park <lightning350@naver.com>
2020-03-22 13:29:27 +0900

    Sort

---
+++ BaekjoonProject/src/sort/Sort_1026.java
@@ -0,0 +1,26 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sort_1026 {
+	public static void main(String[] args) throws Exception {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] arrA = new int[n];
+		int[] arrB = new int[n];
+		for(int i=0; i<n; i++) {
+			arrA[i] = scan.nextInt();
+		}
+		for(int i=0; i<n; i++) {
+			arrB[i] = scan.nextInt();
+		}
+		Arrays.sort(arrA);
+		Arrays.sort(arrB);
+		int sum = 0;
+		for(int i=0; i<n; i++) {
+			sum += arrA[i]*arrB[n-i-1];
+		}
+		System.out.println(sum);
+	}
+}
