6d80cc1b619540915e4b67326234691442aa68d3
hyeseon-park <lightning350@naver.com>
2020-03-24 13:09:38 +0900

    DynamicProgramming 1932번 정수 삼각형

--- BaekjoonProject/src/dynamicProgramming/DynamicProgramming_1932.java
+++ BaekjoonProject/src/dynamicProgramming/DynamicProgramming_1932.java
@@ -6,16 +6,17 @@ public class DynamicProgramming_1932 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int[][] arr = new int[n+1][n+1];
+		int[][] arr = new int[n + 1][n + 1];
 		arr[1][1] = scan.nextInt();
-		for(int i=2; i<=n; i++) {
-			for(int j=1; j<=i; j++) {
-				arr[i][j] = scan.nextInt()+Math.max(arr[i-1][j-1], arr[i-1][j]);
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				arr[i][j] = scan.nextInt() + Math.max(arr[i - 1][j - 1], arr[i - 1][j]);
 			}
 		}
+		scan.close();
 		int max = 0;
-		for(int i=1; i<=n; i++) {
-			if(max<arr[n][i]) {
+		for (int i = 1; i <= n; i++) {
+			if (max < arr[n][i]) {
 				max = arr[n][i];
 			}
 		}
