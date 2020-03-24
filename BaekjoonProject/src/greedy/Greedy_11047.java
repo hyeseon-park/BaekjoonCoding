a38e80c003887d5d76797a2baa44dfac678ba7c8
hyeseon-park <lightning350@naver.com>
2020-03-24 12:56:43 +0900

    Greedy 11047번 동전 0

--- BaekjoonProject/src/greedy/Greedy_11047.java
+++ BaekjoonProject/src/greedy/Greedy_11047.java
@@ -2,17 +2,18 @@ package greedy;
 
 import java.util.Scanner;
 
-public class Greedy_11047 { //동전 0
+public class Greedy_11047 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int count = 0;
-		int n = scan.nextInt(); //10
-		int k = scan.nextInt(); //4200
+		int n = scan.nextInt();
+		int k = scan.nextInt();
 		int[] arr = new int[n];
 		
 		for(int i=0; i<n; i++) {
 			arr[i] = scan.nextInt();
 		}
+		scan.close();
 		for(int i=n-1; i>=0; i--) {
 			if(arr[i] <= k) {
 				count += k/arr[i];
