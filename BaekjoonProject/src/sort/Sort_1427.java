e49c959762c3eb968fc09083256e80d04b43a847
hyeseon-park <lightning350@naver.com>
2020-03-26 18:49:29 +0900

    Sort 1427번 소트인사이드

--- BaekjoonProject/src/sort/Sort_1427.java
+++ BaekjoonProject/src/sort/Sort_1427.java
@@ -1,29 +1,25 @@
 package sort;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Sort_1427 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String n = scan.next();
-		int[] arr = new int[n.length()];
-		for(int i=0; i<n.length(); i++) {
-			arr[i] = n.charAt(i)-'0';
+
+		String number = scan.next();
+		scan.close();
+
+		Integer[] arr = new Integer[number.length()];
+		for (int i = 0; i < number.length(); i++) {
+			arr[i] = number.charAt(i) - '0';
 		}
-		for(int i=0; i<n.length()-1; i++) {
-			for(int j=i+1; j<n.length(); j++) {				
-				if(arr[i]<arr[j]) {
-					int tmp = arr[j];
-					arr[j] = arr[i];
-					arr[i] = tmp;
-				}
-			}
+
+		Arrays.sort(arr, Collections.reverseOrder());
+
+		for (int digit : arr) {
+			System.out.print(digit);
 		}
-		String ans = "";
-		for(int tmp : arr) {
-			ans += tmp;
-		}
-		System.out.print(ans);
 	}
 }
