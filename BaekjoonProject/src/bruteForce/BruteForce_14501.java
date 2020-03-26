1ade930d6be63056eb85c65c82245c240733727a
hyeseon-park <lightning350@naver.com>
2020-03-26 12:46:05 +0900

    BruteForce 14501번 퇴사

--- BaekjoonProject/src/bruteForce/BruteForce_14501.java
+++ BaekjoonProject/src/bruteForce/BruteForce_14501.java
@@ -2,27 +2,27 @@ package bruteForce;
 
 import java.util.Scanner;
 
-public class BruteForce_14501 { //Dynamic Programming
-
+public class BruteForce_14501 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt(); //1 <= n <= 15
-		int[][] arr = new int[n+2][2];
-		int[] ans = new int[n+2];
-		
-		for(int i=1; i<=n; i++) {
-			arr[i][0] = scan.nextInt(); //T
-			arr[i][1] = scan.nextInt(); //P
+		int days = scan.nextInt();
+		int[][] schedule = new int[days + 2][2];
+		int[] profit = new int[days + 2];
+
+		for (int i = 1; i <= days; i++) {
+			schedule[i][0] = scan.nextInt();
+			schedule[i][1] = scan.nextInt();
 		}
-		
-		for(int i=n; i>0; i--) {
-			int day = i+arr[i][0];
-			if(day <= n+1) {
-				ans[i] = Math.max(arr[i][1]+ans[day], ans[i+1]);
-			} else { //상담일 초과
-				ans[i] = ans[i+1];
+		scan.close();
+
+		for (int i = days; i > 0; i--) {
+			int day = i + schedule[i][0];
+			if (day <= days + 1) {
+				profit[i] = Math.max(schedule[i][1] + profit[day], profit[i + 1]);
+			} else {
+				profit[i] = profit[i + 1];
 			}
 		}
-		System.out.println(ans[1]);
+		System.out.println(profit[1]);
 	}
 }
