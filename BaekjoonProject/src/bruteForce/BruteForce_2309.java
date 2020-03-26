8ba9816693286ef318fbd63c17e7cbcfd9fc6531
hyeseon-park <lightning350@naver.com>
2020-03-26 12:35:00 +0900

    BruteForce 2309번 일곱 난쟁이

--- BaekjoonProject/src/bruteForce/BruteForce_2309.java
+++ BaekjoonProject/src/bruteForce/BruteForce_2309.java
@@ -3,44 +3,44 @@ package bruteForce;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BruteForce_2309 { //combination
-	static int[] nine = new int[9];
-	static int[] seven = new int[7];
+public class BruteForce_2309 {
+	static int[] sevenDwarfs = new int[7];
+	static int[] nineDwarfs = new int[9];
+	static int[] arr = new int[7];
 	static boolean isFirst = true;
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int[] arr = new int[9];
 		for(int i=0; i<9; i++) {
-			nine[i] = scan.nextInt();
+			nineDwarfs[i] = scan.nextInt();
 		}
-		combination(arr, 9, 7, 0, 0); //9개 중 7개
+		scan.close();
+		combination(9, 7, 0, 0);
 	}
-	public static void combination(int[] comArr, int n, int r, int index, int target) {
+	
+	public static void combination(int n, int r, int index, int target) {
 		if(!isFirst) {
 			return;
 		}
 		if(r == 0) {
 			int sum = 0;
 			for(int i=0; i<index; i++) {
-				seven[i] = nine[comArr[i]];
-				sum += seven[i]; 
+				sevenDwarfs[i] = nineDwarfs[arr[i]];
+				sum += sevenDwarfs[i]; 
 			}
-			
 			if(sum == 100) {
-				Arrays.sort(seven);
-				for(int tmp : seven) {
-					System.out.println(tmp);
+				Arrays.sort(sevenDwarfs);
+				for(int dwarf : sevenDwarfs) {
+					System.out.println(dwarf);
 				}
 				isFirst = false;
 			}
-			
 		} else if(target == n) {
 			return;
-		} else { //처음 들어오는 곳
-			comArr[index] = target; 
-			combination(comArr, n, r-1, index+1, target+1); //
-			combination(comArr, n, r, index, target+1);
-			//반복 후에 comArr는 0부터 6까지의 숫자가 들어가있음
+		} else {
+			arr[index] = target; 
+			combination(n, r-1, index+1, target+1);
+			combination(n, r, index, target+1);
 		}
 	}
 }
