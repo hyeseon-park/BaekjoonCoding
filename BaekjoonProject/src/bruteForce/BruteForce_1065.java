b1a02e89b5c60ed46db8e7191075310229934fd2
hyeseon-park <lightning350@naver.com>
2020-03-26 12:21:39 +0900

    BruteForce 1065번 한수

--- BaekjoonProject/src/bruteForce/BruteForce_1065.java
+++ BaekjoonProject/src/bruteForce/BruteForce_1065.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class BruteForce_1065 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt(); // <= 1000
 		int count = 0;
-		for(int i=1; i<=n; i++) {
-			if(i<100) {
+		int n = scan.nextInt();
+		scan.close();
+
+		for (int i = 1; i <= n; i++) {
+			if (i < 100) {
 				count++;
-			} else if(i<1000) {				
-				int num1 = i/100%10; //세번째 자리
-				int num2 = i/10%10; //두번째 자리 
-				int num3 = i%10; //첫번째 자리
-				if(num2*2 == num1+num3) {
+			} else if (i < 1000) {
+				int num1 = i / 100 % 10;
+				int num2 = i / 10 % 10;
+				int num3 = i % 10;
+				if (num2 * 2 == num1 + num3) {
 					count++;
 				}
 			}
