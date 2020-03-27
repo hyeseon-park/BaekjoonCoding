fffc545ce8fa320c3da6dd6613f8a8d65ae963fe
hyeseon-park <lightning350@naver.com>
2020-03-27 16:03:44 +0900

    BruteForce 1182번 부분수열의 합

--- BaekjoonProject/src/bruteForce/BruteForce_1182.java
+++ BaekjoonProject/src/bruteForce/BruteForce_1182.java
@@ -3,31 +3,34 @@ package bruteForce;
 import java.util.Scanner;
 
 public class BruteForce_1182 {
-	public static int[] arr;
-	public static int n, s, count;
+	public static int num, sum, count;
+	public static int[] sequence;
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		count = 0;
-		n = scan.nextInt();
-		s = scan.nextInt();
-		arr = new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = scan.nextInt();
+		num = scan.nextInt();
+		sum = scan.nextInt();
+		sequence = new int[num];
+		for (int i = 0; i < num; i++) {
+			sequence[i] = scan.nextInt();
 		}
-		cal(0, 0);
-		if(s == 0) {
+		scan.close();
+
+		getSubsequence(0, 0);
+		if (sum == 0)
 			count -= 1;
-		}
+
 		System.out.println(count);
 	}
-	public static void cal(int index, int result) {
-		if(index == n) {
-			if(result == s) {
+
+	public static void getSubsequence(int index, int result) {
+		if (index == num) {
+			if (result == sum) {
 				count++;
 			}
 			return;
 		}
-		cal(index+1, result+arr[index]);
-		cal(index+1, result);
+		getSubsequence(index + 1, result + sequence[index]);
+		getSubsequence(index + 1, result);
 	}
 }
