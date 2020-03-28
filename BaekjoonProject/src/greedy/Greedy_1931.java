7fcc09e31ae986a0a4877ba277ebee849b95a647
hyeseon-park <lightning350@naver.com>
2020-03-28 22:18:24 +0900

    Greedy 1931번 회의실배정

--- BaekjoonProject/src/greedy/Greedy_1931.java
+++ BaekjoonProject/src/greedy/Greedy_1931.java
@@ -4,37 +4,39 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-public class Greedy_1931 { //회의실 배정
+public class Greedy_1931 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int n = scan.nextInt();
 		int[][] time = new int[n][2];
 		int count = 0;
-		
-		for(int i=0; i<n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			time[i][0] = scan.nextInt();
 			time[i][1] = scan.nextInt();
 		}
-		
+		scan.close();
+
 		Arrays.sort(time, new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
-				if(o1[1] == o2[1]) {
-					return o1[0]-o2[0]; //같을 경우 시작 시간을 기준으로 정렬
+				if (o1[1] == o2[1]) {
+					return o1[0] - o2[0];
 				} else {
-					return o1[1]-o2[1]; //종료 시간을 기준으로 정렬
+					return o1[1] - o2[1];
 				}
 			}
 		});
-		//끝나는 시간으로 정렬
-		
+
 		int end = -1;
-		for(int i=0; i<n; i++) {
-			if(time[i][0] >= end) {
+		for (int i = 0; i < n; i++) {
+			if (time[i][0] >= end) {
 				end = time[i][1];
 				count++;
 			}
 		}
+
 		System.out.println(count);
 	}
 }
