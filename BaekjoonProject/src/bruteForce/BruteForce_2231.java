c5e52309fa3c81a76102aeeaf15c015dcbe48435
hyeseon-park <lightning350@naver.com>
2020-03-28 13:53:14 +0900

    BruteForce 2231번 분해합

--- BaekjoonProject/src/bruteForce/BruteForce_2231.java
+++ BaekjoonProject/src/bruteForce/BruteForce_2231.java
@@ -5,22 +5,28 @@ import java.util.Scanner;
 public class BruteForce_2231 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int n = scan.nextInt();
+		scan.close();
+
 		int answer = 0;
-		for(int i=n; i>=1; i--) {	
-			int tmpNum = i + sum(i);
-			if(n == tmpNum) {
+		for (int i = n; i >= 1; i--) {
+			int decomposition = i + getSum(i);
+			if (n == decomposition) {
 				answer = i;
 			}
 		}
+		
 		System.out.println(answer);
 	}
-	public static int sum(int n) {
+
+	public static int getSum(int n) {
 		int sum = 0;
-		while(n >= 1) {
-			sum += n%10;
-			n = n/10;
+		while (n >= 1) {
+			sum += n % 10;
+			n = n / 10;
 		}
+		System.out.println(sum);
 		return sum;
 	}
 }
