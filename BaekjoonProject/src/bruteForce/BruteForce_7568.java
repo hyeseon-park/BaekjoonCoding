30b9a5031f7d12f3f03f7c07f580a9365cd35bf1
hyeseon-park <lightning350@naver.com>
2020-03-28 13:58:52 +0900

    BruteForce 7568번 덩치

--- BaekjoonProject/src/bruteForce/BruteForce_7568.java
+++ BaekjoonProject/src/bruteForce/BruteForce_7568.java
@@ -2,27 +2,31 @@ package bruteForce;
 
 import java.util.Scanner;
 
-public class BruteForce_7568 { //덩치
+public class BruteForce_7568 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int n = scan.nextInt();
-		int[][] arr = new int[n][2];
-		int[] rank = new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i][0] = scan.nextInt();
-			arr[i][1] = scan.nextInt();
+		int[][] bodies = new int[n][2];
+		int[] ranks = new int[n];
+		for (int i = 0; i < n; i++) {
+			bodies[i][0] = scan.nextInt();
+			bodies[i][1] = scan.nextInt();
 		}
-		for(int i=0; i<n-1; i++) {
-			for(int j=i+1; j<n; j++) {
-				if(arr[i][0]<arr[j][0] && arr[i][1]<arr[j][1]) {
-					rank[i] += 1;
-				} else if(arr[i][0]>arr[j][0] && arr[i][1]>arr[j][1]) {
-					rank[j] += 1;
-				} 
+		scan.close();
+
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (bodies[i][0] < bodies[j][0] && bodies[i][1] < bodies[j][1]) {
+					ranks[i] += 1;
+				} else if (bodies[i][0] > bodies[j][0] && bodies[i][1] > bodies[j][1]) {
+					ranks[j] += 1;
+				}
 			}
 		}
-		for(int tmp : rank) {			
-			System.out.print(tmp+1+" ");
+
+		for (int rank : ranks) {
+			System.out.print(rank + 1 + " ");
 		}
 	}
 }
