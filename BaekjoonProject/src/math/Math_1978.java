4e2d0939c5a7df1b5f92d4d368201dda1fab7f61
hyeseon-park <lightning350@naver.com>
2020-03-28 22:13:44 +0900

    Math 1978번 소수 찾기

--- BaekjoonProject/src/math/Math_1978.java
+++ BaekjoonProject/src/math/Math_1978.java
@@ -3,26 +3,30 @@ package math;
 import java.util.Scanner;
 
 public class Math_1978 {
-	public static void main(String[] args) { //소수 찾기
+	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int n = scan.nextInt();
 		int count = 0;
-		for(int i=0; i<n; i++) {
-			int num = scan.nextInt(); // 1 3 5 7
+		for (int i = 0; i < n; i++) {
+			int num = scan.nextInt();
 			boolean isPrime = true;
-			for(int j=2; j<num; j++) {
-				if(num%j==0) {
+
+			if (num == 1) {
+				isPrime = false;
+			}
+			for (int j = 2; j < num; j++) {
+				if (num % j == 0) {
 					isPrime = false;
 					break;
 				}
 			}
-			if(num == 1) {
-				isPrime = false;
-			}
-			if(isPrime) {				
+			if (isPrime) {
 				count++;
 			}
 		}
+		scan.close();
+
 		System.out.println(count);
 	}
 }
