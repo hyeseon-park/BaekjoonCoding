d9e1f05f597f8161fd59f3e118e15487d166cd84
hyeseon-park <lightning350@naver.com>
2020-03-28 22:10:25 +0900

    Math 1712번 손익분기점

--- BaekjoonProject/src/math/Math_1712.java
+++ BaekjoonProject/src/math/Math_1712.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Math_1712 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
-		if(b >= c) {
+		scan.close();
+		
+		if (b >= c) {
 			System.out.println(-1);
 		} else {
-			System.out.println(a/(c-b)+1);
+			System.out.println(a / (c - b) + 1);
 		}
 	}
 }
