82334a44197063ce796bd8f2861ffb54a3560725
hyeseon-park <lightning350@naver.com>
2020-03-28 23:18:18 +0900

    DfsBfs 9372번 상근이의 여행

--- BaekjoonProject/src/dfsBfs/DfsBfs_9372.java
+++ BaekjoonProject/src/dfsBfs/DfsBfs_9372.java
@@ -1,7 +1,21 @@
 package dfsBfs;
 
-public class DfsBfs_9372 { //상근이의 여행
+import java.util.Scanner;
+
+public class DfsBfs_9372 {
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+
+		int testCase = scan.nextInt();
+		for (int i = 0; i < testCase; i++) {
+			int nations = scan.nextInt();
+			int planes = scan.nextInt();
+			for (int j = 0; j < planes; j++) {
+				scan.nextInt();
+				scan.nextInt();
+			}
+			System.out.println(nations - 1);
+		}
+		scan.close();
 	}
 }
