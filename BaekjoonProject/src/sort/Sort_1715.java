dafb9df34da9ab8485b900dab4137edc91ee5559
hyeseon-park <lightning350@naver.com>
2020-03-28 20:26:25 +0900

    Sort 1715번 카드 정렬하기

--- BaekjoonProject/src/sort/Sort_1715.java
+++ BaekjoonProject/src/sort/Sort_1715.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 
 public class Sort_1715 {
 	public static void main(String[] args) {
-		int ans = 0;
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		for(int i=0; i<N; i++) {
+
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		int count = 0;
+		int n = scan.nextInt();
+		for (int i = 0; i < n; i++) {
 			pq.offer(scan.nextInt());
 		}
-		while(pq.size() != 1) {
+		scan.close();
+
+		while (pq.size() != 1) {
 			int p = pq.poll();
-		    int q = pq.poll();
-		    ans += p + q;
-		    pq.offer(p + q);
+			int q = pq.poll();
+			count += p + q;
+			pq.offer(p + q);
 		}
-		System.out.println(ans);
+		System.out.println(count);
 	}
 }
