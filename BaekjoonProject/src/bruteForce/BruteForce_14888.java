55bf768d61cf8b3bf77b755e00dda1eef6fd0995
hyeseon-park <lightning350@naver.com>
2020-03-28 13:35:36 +0900

    BruteForce 14888번 연산자 끼워넣기

--- BaekjoonProject/src/bruteForce/BruteForce_14888.java
+++ BaekjoonProject/src/bruteForce/BruteForce_14888.java
@@ -8,45 +8,49 @@ public class BruteForce_14888 {
 	public static int[] operator = new int[4];
 	public static int min = Integer.MAX_VALUE;
 	public static int max = Integer.MIN_VALUE;
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		n = scan.nextInt();
 		num = new int[n];
-		for(int i=0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
 			num[i] = scan.nextInt();
 		}
-		for(int i=0; i<4; i++) {
+		for (int i = 0; i < 4; i++) {
 			operator[i] = scan.nextInt();
 		}
+		scan.close();
+
 		cal(1, num[0]);
 		System.out.println(max);
 		System.out.println(min);
 	}
+
 	public static void cal(int index, int result) {
-		if(index == n) {
+		if (index == n) {
 			min = Math.min(min, result);
 			max = Math.max(max, result);
 			return;
 		}
-		if(operator[0]>0) {
+		if (operator[0] > 0) {
 			operator[0]--;
-			cal(index+1, result+num[index]);
+			cal(index + 1, result + num[index]);
 			operator[0]++;
 		}
-		if(operator[1]>0) {
+		if (operator[1] > 0) {
 			operator[1]--;
-			cal(index+1, result-num[index]);
+			cal(index + 1, result - num[index]);
 			operator[1]++;
 		}
-		if(operator[2]>0) {
+		if (operator[2] > 0) {
 			operator[2]--;
-			cal(index+1, result*num[index]);
+			cal(index + 1, result * num[index]);
 			operator[2]++;
 		}
-		if(operator[3]>0) {
+		if (operator[3] > 0) {
 			operator[3]--;
-			cal(index+1, result/num[index]);
+			cal(index + 1, result / num[index]);
 			operator[3]++;
 		}
 	}
