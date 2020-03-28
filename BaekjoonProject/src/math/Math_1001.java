4e51d792a007c1025d9e80f4d1507a33dd1e969b
hyeseon-park <lightning350@naver.com>
2020-03-28 22:07:06 +0900

    Math 1001번 A-B

--- BaekjoonProject/src/math/Math_1001.java
+++ BaekjoonProject/src/math/Math_1001.java
@@ -7,6 +7,7 @@ public class Math_1001 {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
-		System.out.println(a+b);
+		scan.close();
+		System.out.println(a-b);
 	}
 }
