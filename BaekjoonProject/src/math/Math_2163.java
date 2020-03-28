9b4c68adc069b34722dc1250039c3e0948fe3220
hyeseon-park <lightning350@naver.com>
2020-03-28 22:14:58 +0900

    Math 2163번 초콜릿 자르기

--- BaekjoonProject/src/math/Math_2163.java
+++ BaekjoonProject/src/math/Math_2163.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Math_2163 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int n = scan.nextInt();
 		int m = scan.nextInt();
-		System.out.println((n-1)+((m-1)*n));
+		scan.close();
+
+		System.out.println((n - 1) + ((m - 1) * n));
 	}
 }
