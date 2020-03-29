997ce5a3114acb45f1d98ad1950a5b973f33591b
hyeseon-park <lightning350@naver.com>
2020-03-29 14:41:03 +0900

    DfsBfs 10451번 순열 사이클

--- BaekjoonProject/src/dfsBfs/DfsBfs_10451.java
+++ BaekjoonProject/src/dfsBfs/DfsBfs_10451.java
@@ -3,34 +3,39 @@ package dfsBfs;
 import java.util.Scanner;
 
 public class DfsBfs_10451 {
-   static int[] arr;
-   static boolean[] visited;
-   static int n;
-   public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      int testCase = scan.nextInt();
-      int cnt;
-      for(int i = 0; i < testCase; i++) {
-         n = scan.nextInt(); 
-         cnt = 0;
-         arr = new int[n+1]; 
-         visited = new boolean[n+1];
-         for(int j = 1; j < n+1; j++) {
-        	 arr[j] = scan.nextInt();        	 
-         }
-         for(int j = 1; j < n+1; j++) {
-            if(!visited[j]) {
-            	cnt += dfs(j);
-            }
-         }
-         System.out.println(cnt);
-      }
-   }
-   public static int dfs(int i) {
-      if(i == arr[i] || visited[i]) {
-    	  return 1;
-      }
-      visited[i] = true;
-      return dfs(arr[i]);
-   }
+	static int n;
+	static int[] arr;
+	static boolean[] visited;
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int testCase = scan.nextInt();
+		for (int i = 0; i < testCase; i++) {
+			int count = 0;
+			n = scan.nextInt();
+			arr = new int[n + 1];
+			visited = new boolean[n + 1];
+			for (int j = 1; j < n + 1; j++) {
+				arr[j] = scan.nextInt();
+			}
+			scan.close();
+
+			for (int j = 1; j < n + 1; j++) {
+				if (!visited[j]) {
+					count += dfs(j);
+				}
+			}
+			
+			System.out.println(count);
+		}
+	}
+
+	public static int dfs(int i) {
+		if (i == arr[i] || visited[i]) {
+			return 1;
+		}
+		visited[i] = true;
+		return dfs(arr[i]);
+	}
 }
