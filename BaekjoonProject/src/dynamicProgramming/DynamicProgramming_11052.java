b781b788d8e0cba53d30eb477141b4268f16c9f1
hyeseon-park <lightning350@naver.com>
2020-03-29 17:12:58 +0900

    DynamicProgramming 11052번 카드 구매하기

--- BaekjoonProject/src/dynamicProgramming/DynamicProgramming_11052.java
+++ BaekjoonProject/src/dynamicProgramming/DynamicProgramming_11052.java
@@ -2,25 +2,29 @@ package dynamicProgramming;
 
 import java.util.Scanner;
 
-public class DynamicProgramming_11052 { //카드 구매하기
+public class DynamicProgramming_11052 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int n = scan.nextInt();
-		int[] arr = new int[n+1];
-		int[] accum = new int[n+1];
-		for(int i=1; i<=n; i++) {
-			arr[i] = scan.nextInt();
+		int[] prices = new int[n + 1];
+		int[] accum = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			prices[i] = scan.nextInt();
 		}
-		for(int i=1; i<=n; i++) {
-			int max = 0;
-			for(int j=1; j<i; j++) {
-				int tmpPrice = accum[i-j]+arr[j];
-				if(max<tmpPrice) {
-					max = tmpPrice;
+		scan.close();
+
+		for (int i = 1; i <= n; i++) {
+			int maxPrice = 0;
+			for (int j = 1; j < i; j++) {
+				int tmpPrice = accum[i - j] + prices[j];
+				if (maxPrice < tmpPrice) {
+					maxPrice = tmpPrice;
 				}
 			}
-			accum[i] = Math.max(arr[i], max);
+			accum[i] = Math.max(prices[i], maxPrice);
 		}
+
 		System.out.println(accum[n]);
 	}
 }
