4f19e24ea81821b3a40494a894ec399c2280a015
hyeseon-park <lightning350@naver.com>
2020-03-29 13:42:03 +0900

    DfsBfs 2606번 바이러스

--- BaekjoonProject/src/dfsBfs/DfsBfs_2606.java
+++ BaekjoonProject/src/dfsBfs/DfsBfs_2606.java
@@ -3,31 +3,36 @@ package dfsBfs;
 import java.util.Scanner;
 
 public class DfsBfs_2606 {
-    static int N_com, N_conn;
-    static int map[][];
-    static int cnt;
-    static int visited[];
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        N_com = scan.nextInt();
-        N_conn = scan.nextInt();
-        map = new int[N_com+1][N_com+1];
-        visited = new int[N_com+1];
-        for(int i=0; i<N_conn; i++) {
-            int a = scan.nextInt();
-            int b = scan.nextInt();
-            map[a][b] = map[b][a] = 1;    
-        }
-        dfs(1);
-        System.out.println(cnt);
-    }
-    static void dfs(int start) {
-        visited[start] = 1;
-        for(int i=1; i<N_com+1; i++) {
-            if(map[start][i] == 1 && visited[i] == 0) {
-                dfs(i);
-                cnt++;
-            }
-        }
-    }
+	static int computer, connection;
+	static int map[][];
+	static int visited[];
+	static int count;
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		computer = scan.nextInt();
+		connection = scan.nextInt();
+		map = new int[computer + 1][computer + 1];
+		visited = new int[computer + 1];
+		for (int i = 0; i < connection; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			map[a][b] = map[b][a] = 1;
+		}
+		scan.close();
+
+		dfs(1);
+		System.out.println(count);
+	}
+
+	static void dfs(int start) {
+		visited[start] = 1;
+		for (int i = 1; i < computer + 1; i++) {
+			if (map[start][i] == 1 && visited[i] == 0) {
+				dfs(i);
+				count++;
+			}
+		}
+	}
 }
