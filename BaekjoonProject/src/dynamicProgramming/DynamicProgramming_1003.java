dcc056612aa7170f06948f0fbd4f9c08eab8b883
hyeseon-park <lightning350@naver.com>
2020-03-29 16:33:50 +0900

    DynamicProgramming 1003번 피보나치 함수

--- BaekjoonProject/src/dynamicProgramming/DynamicProgramming_1003.java
+++ BaekjoonProject/src/dynamicProgramming/DynamicProgramming_1003.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 public class DynamicProgramming_1003 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int t = scan.nextInt();
+		
+		int testCase = scan.nextInt();
 		int[][] arr = new int[41][2];
+		
 		arr[0][0] = 1;
 		arr[1][1] = 1;
-		for(int i=0; i<t; i++) {
+		for (int i = 0; i < testCase; i++) {
 			int n = scan.nextInt();
-			for(int j=2; j<=n; j++) {
-				arr[j][0] = arr[j-1][0]+arr[j-2][0]; 
-				arr[j][1] = arr[j-1][1]+arr[j-2][1]; 
+			for (int j = 2; j <= n; j++) {
+				arr[j][0] = arr[j - 1][0] + arr[j - 2][0];
+				arr[j][1] = arr[j - 1][1] + arr[j - 2][1];
 			}
-			System.out.println(arr[n][0]+" "+arr[n][1]);
+			System.out.println(arr[n][0] + " " + arr[n][1]);
 		}
+		scan.close();
 	}
 }
