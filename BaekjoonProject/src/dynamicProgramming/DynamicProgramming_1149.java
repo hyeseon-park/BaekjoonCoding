c6606c3a59b7534b3ed7bbcda0bd31a1bce5e668
hyeseon-park <lightning350@naver.com>
2020-03-30 12:08:37 +0900

    DynamicProgramming 1149번 RGB거리

--- BaekjoonProject/src/dynamicProgramming/DynamicProgramming_1149.java
+++ BaekjoonProject/src/dynamicProgramming/DynamicProgramming_1149.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class DynamicProgramming_1149 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
 		int n = scan.nextInt();
-		int[][] ans = new int[n+1][3];
-		ans[0][0]=ans[0][1]=ans[0][2]=0;
-		for(int i=1; i<=n; i++) {
-			ans[i][0] = scan.nextInt()+Math.min(ans[i-1][1], ans[i-1][2]);
-			ans[i][1] = scan.nextInt()+Math.min(ans[i-1][0], ans[i-1][2]);
-			ans[i][2] = scan.nextInt()+Math.min(ans[i-1][0], ans[i-1][1]);
+		int[][] rgb = new int[n + 1][3];
+		rgb[0][0] = rgb[0][1] = rgb[0][2] = 0;
+		for (int i = 1; i <= n; i++) {
+			rgb[i][0] = scan.nextInt() + Math.min(rgb[i - 1][1], rgb[i - 1][2]);
+			rgb[i][1] = scan.nextInt() + Math.min(rgb[i - 1][0], rgb[i - 1][2]);
+			rgb[i][2] = scan.nextInt() + Math.min(rgb[i - 1][0], rgb[i - 1][1]);
 		}
-		System.out.println(Math.min(ans[n][0], Math.min(ans[n][1], ans[n][2])));
+		scan.close();
+		
+		System.out.println(Math.min(rgb[n][0], Math.min(rgb[n][1], rgb[n][2])));
 	}
 }
