fc232fdbbc59433eb0802f9cf9d2c2d2b62b0ff6
hyeseon-park <lightning350@naver.com>
2020-03-31 11:31:52 +0900

    DynamicProgramming 11727번 2×n 타일링 2

--- BaekjoonProject/src/dynamicProgramming/DynamicProgramming_11727.java
+++ BaekjoonProject/src/dynamicProgramming/DynamicProgramming_11727.java
@@ -2,20 +2,24 @@ package dynamicProgramming;
 
 import java.util.Scanner;
 
-public class DynamicProgramming_11727 { //2xn 타일링 2
+public class DynamicProgramming_11727 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int n = scan.nextInt();
-		int[] tiles = new int[n+1];	
-		for(int i=1; i<=n; i++) {
-			if(i==1) {
+		scan.close();
+
+		int[] tiles = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			if (i == 1) {
 				tiles[1] = 1;
-			} else if(i==2) {
+			} else if (i == 2) {
 				tiles[2] = 3;
-			} else {				
-				tiles[i] = (tiles[i-1]+tiles[i-2]*2)%10007;
+			} else {
+				tiles[i] = (tiles[i - 1] + tiles[i - 2] * 2) % 10007;
 			}
 		}
+
 		System.out.println(tiles[n]);
 	}
 }
