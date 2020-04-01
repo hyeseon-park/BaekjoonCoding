87a930a48a8c3d9f9c1dbcab03a0eeb762b31299
hyeseon-park <lightning350@naver.com>
2020-04-01 17:16:20 +0900

    DynamicProgramming 1463번 1로 만들기

--- BaekjoonProject/src/dynamicProgramming/DynamicProgramming_1463.java
+++ BaekjoonProject/src/dynamicProgramming/DynamicProgramming_1463.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 public class DynamicProgramming_1463 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int x = scan.nextInt();
-		int[] arr = new int[x+1];
-		arr[1] = 0;
-		for(int i=2; i<=x; i++) {			
-			int case1 = 1000000;
-			int case2 = 1000000;
-			int case3 = 1000000;
-			if(i%3==0) {
-				case1 = arr[i/3]+1;
-			} 
-			if(i%2==0) {
-				case2 = arr[i/2]+1;
-			} 
-			if(i-1!=0){
-				case3 = arr[i-1]+1;
+
+		int num = scan.nextInt();
+		scan.close();
+
+		int[] counts = new int[num + 1];
+		counts[1] = 0;
+		for (int i = 2; i <= num; i++) {
+			int division3 = 1000000; 
+			int division2 = 1000000;
+			int subtraction1 = 1000000; 
+			if (i % 3 == 0) {
+				division3 = counts[i / 3] + 1;
 			}
-			arr[i] = Math.min(case1, Math.min(case2, case3));
+			if (i % 2 == 0) {
+				division2 = counts[i / 2] + 1;
+			}
+			if (i - 1 != 0) {
+				subtraction1 = counts[i - 1] + 1;
+			}
+			counts[i] = Math.min(division3, Math.min(division2, subtraction1));
 		}
-		System.out.println(arr[x]);
+
+		System.out.println(counts[num]);
 	}
 }
