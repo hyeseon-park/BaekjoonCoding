7694fd5d674c9ebb58580c9f9414dffa8f4c9808
hyeseon-park <lightning350@naver.com>
2020-04-02 14:00:35 +0900

    DynamicProgramming 9095번 1, 2, 3 더하기

--- BaekjoonProject/src/dynamicProgramming/DynamicProgramming_9095.java
+++ BaekjoonProject/src/dynamicProgramming/DynamicProgramming_9095.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class DynamicProgramming_9095 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
+		int testCase = scan.nextInt();
 		int[] arr = new int[11];
 		arr[1] = 1;
 		arr[2] = 2;
 		arr[3] = 4;
-		int testCase = scan.nextInt();
-		for(int i=0; i<testCase; i++) {
+		
+		for (int i = 0; i < testCase; i++) {
 			int n = scan.nextInt();
-			for(int j=4; j<=n; j++) {			
-				arr[j] = arr[j-1]+arr[j-2]+arr[j-3];
+			for (int j = 4; j <= n; j++) {
+				arr[j] = arr[j - 1] + arr[j - 2] + arr[j - 3];
 			}
 			System.out.println(arr[n]);
 		}
+		scan.close();
 	}
 }
-
