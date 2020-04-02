e014b735b7b1213f389221c83bd06737f84f12ca
hyeseon-park <lightning350@naver.com>
2020-04-02 13:58:07 +0900

    DynamicProgramming 2579번 계단 오르기

--- BaekjoonProject/src/dynamicProgramming/DynamicProgramming_2579.java
+++ BaekjoonProject/src/dynamicProgramming/DynamicProgramming_2579.java
@@ -5,23 +5,27 @@ import java.util.Scanner;
 public class DynamicProgramming_2579 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+
 		int stair = scan.nextInt();
-		int[] stairs = new int[stair+1];
-		int[] newStairs = new int[stair+1];
-		for(int i=1; i<=stair; i++) {
+		int[] stairs = new int[stair + 1];
+		int[] accum = new int[stair + 1];
+		for (int i = 1; i <= stair; i++) {
 			stairs[i] = scan.nextInt();
 		}
-		for(int i=1; i<=stair; i++) { //2가지 방법이 있음	
-			if(i==1) {
-				newStairs[1] = stairs[1];
-			} else if(i==2) {
-				newStairs[2] = stairs[2] + stairs[1];
-			} else {	
-				int noJump = stairs[i]+stairs[i-1]+newStairs[i-3]; //1계단 전+3계단 전까지의 누적
-				int jump = stairs[i]+newStairs[i-2]; //2계단 전까지의 누적
-				newStairs[i] = Math.max(noJump, jump); //2가지 방법을 비교해서 큰 쪽으로
+		scan.close();
+
+		for (int i = 1; i <= stair; i++) {
+			if (i == 1) {
+				accum[1] = stairs[1];
+			} else if (i == 2) {
+				accum[2] = stairs[2] + stairs[1];
+			} else {
+				int noJump = stairs[i] + stairs[i - 1] + accum[i - 3];
+				int jump = stairs[i] + accum[i - 2];
+				accum[i] = Math.max(noJump, jump);
 			}
 		}
-		System.out.println(newStairs[stair]);
+
+		System.out.println(accum[stair]);
 	}
 }
