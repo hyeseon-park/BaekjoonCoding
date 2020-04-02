07f6fd64ece6ce86e3b824b7553efbf6fc0bdca2
hyeseon-park <lightning350@naver.com>
2020-04-02 13:56:18 +0900

    DynamicProgramming 2193번 이친수

--- BaekjoonProject/src/dynamicProgramming/DynamicProgramming_2193.java
+++ BaekjoonProject/src/dynamicProgramming/DynamicProgramming_2193.java
@@ -5,24 +5,18 @@ import java.util.Scanner;
 public class DynamicProgramming_2193 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-//		solution(n);
-		long[][] nArr = new long[n+1][2];
-		nArr[1][0] = 0; //0
-		nArr[1][1] = 1; //1
-		for(int i=2; i<=n; i++) {
-			nArr[i][0] = nArr[i-1][0]+nArr[i-1][1];
-			nArr[i][1] = nArr[i-1][0];
+
+		int digit = scan.nextInt();
+		scan.close();
+		
+		long[][] pinaryNum = new long[digit + 1][2];
+		pinaryNum[1][0] = 0;
+		pinaryNum[1][1] = 1;
+		for (int i = 2; i <= digit; i++) {
+			pinaryNum[i][0] = pinaryNum[i - 1][0] + pinaryNum[i - 1][1];
+			pinaryNum[i][1] = pinaryNum[i - 1][0];
 		}
-		System.out.println(nArr[n][0]+nArr[n][1]);
-	} 
-	static void solution(int n) {
-		long[] arr = new long[n+1];
-		arr[0] = 0;
-		arr[1] = 1;
-		for(int i=2; i<=n; i++) {			
-			arr[i] = arr[i-1]+arr[i-2];
-		}
-		System.out.println(arr[n]);
+
+		System.out.println(pinaryNum[digit][0] + pinaryNum[digit][1]);
 	}
 }
