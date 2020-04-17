8bd7dd2d3cb28e7966db189e6232e5dd30dc2d02
hyeseon-park <lightning350@naver.com>
2020-04-17 23:37:36 +0900

    Sort 10989번 수 정렬하기 3

--- BaekjoonProject/src/sort/Sort_10989.java
+++ BaekjoonProject/src/sort/Sort_10989.java
@@ -8,12 +8,15 @@ import java.io.OutputStreamWriter;
 public class Sort_10989 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[10001];
+		
 		for(int i=0; i<n; i++) {
 			arr[Integer.parseInt(br.readLine())]++;
 		}
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		for(int i=1; i<=10000; i++) {
 			if(arr[i]>0) {
 				for(int j=0; j<arr[i]; j++) {
