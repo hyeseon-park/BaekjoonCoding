ef7eff6163be795aa61e38e29abf9b3f84a88964
hyeseon-park <lightning350@naver.com>
2020-06-25 00:16:14 +0900

    Stack 10799번 쇠막대기

--- BaekjoonProject/src/stack/Stack_10799.java
+++ BaekjoonProject/src/stack/Stack_10799.java
@@ -7,22 +7,23 @@ public class Stack_10799 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		Stack<Character> stack = new Stack<Character>();
-		String bracket = scan.next();
+		String str = scan.next();
 		scan.close();
 
 		int count = 0;
-		for (int i = 0; i < bracket.length(); i++) {
-			char b = bracket.charAt(i);
+		Stack<Character> stack = new Stack<Character>();
 
-			if (b == '(') {
-				stack.push(b);
-			} else if (b == ')') {
-				if (stack.peek() == '(') {
-					stack.pop();
+		for (int i = 0; i < str.length(); i++) {
+			char bracket = str.charAt(i);
+
+			if (bracket == '(') {
+				stack.push(bracket);
+			} else if (bracket == ')') {
+				stack.pop();
+				
+				if (str.charAt(i-1) == '(') {
 					count += stack.size();
 				} else {
-					stack.pop();
 					count++;
 				}
 			}
