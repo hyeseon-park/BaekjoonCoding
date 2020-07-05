e178112d4795e6a39ad3ec8ec4816240af252eac
hyeseon-park <lightning350@naver.com>
2020-07-05 13:26:59 +0900

    Backtracking 15649번 N과 M (1)

--- BaekjoonProject/src/backtracking/Backtracking_15649.java
+++ BaekjoonProject/src/backtracking/Backtracking_15649.java
@@ -6,7 +6,6 @@ public class Backtracking_15649 {
 	static int n, m;
 	static int[] numbers;
 	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -19,15 +18,14 @@ public class Backtracking_15649 {
 		visited = new boolean[n + 1];
 
 		backtracking(0);
-		System.out.println(sb);
 	}
 
 	static void backtracking(int count) {
 		if (count == m) {
 			for (int i = 0; i < m; i++) {
-				sb.append(numbers[i] + " ");
+				System.out.print(numbers[i] + " ");
 			}
-			sb.append("\n");
+			System.out.println();
 		}
 
 		for (int i = 1; i <= n; i++) {
